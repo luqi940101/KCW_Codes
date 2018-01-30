843b035b79be4c3551e86be6ae2a21819cd7b426
luqi940101 <luqi940101@gmail.com>
2018-01-29 18:39:04 -0600

    01/29 works

--- 车载防撞预警终端项目代码/KCW/MainWindow.xaml.cs
+++ 车载防撞预警终端项目代码/KCW/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ using System.IO;
 using System.IO.Ports;
 
 
+
+
 namespace WpfApplication4
 {
     /// <summary>
@@ -30,14 +32,52 @@ namespace WpfApplication4
         public MainWindow()
         {
             InitializeComponent();
-            Vehicle1.Stroke = Brushes.Red;
-            Vehicle1.Fill = Brushes.Red;
-         //   Vehicle2.Fill = 
+            
+            //tsmiConnect.Enabled = false;
+            //Application.DoEvents();
+            
+            SystemPub.ADSio = new ADCom();
+            if (SystemPub.ADSio.bConnected)
+            {
+                SystemPub.ADSio.DisConnect();
+               // m_bStopComm = true;
+            }
+            else
+            {
+              //  m_bStopComm = false;
+                SystemPub.ADRcp.Sio = SystemPub.ADSio;
+                SystemPub.ADSio.Connect(IniSettings.HostName, IniSettings.HostPort);
+              //  Application.DoEvents();
+                if (!SystemPub.ADSio.bConnected && IniSettings.Communication == IniSettings.CommType.NET) RFID_Com_Label.Foreground = Brushes.Red;
+                // Application.DoEvents();
+            }
+            //Vehicle1.Stroke = Brushes.Red;
+            //Vehicle1.Fill = Brushes.Red;
+            
         }
+        
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
+            BtnClose.IsEnabled = false;
             System.Windows.Application.Current.Shutdown();
         }
+        
+        private void BtnReadCard_Click(object sender, RoutedEventArgs e)
+        {
+            BtnReadCard.IsEnabled = false;
+            CardID.Text = "";
+            //Application.DoEvents();
+              PassiveCommand.Identify6C(SystemPub.ADRcp);
+            //if (!SystemPub.ADRcp.SendBytePkt(PassiveRcp.Identify6C(SystemPub.ADRcp.Address))) { }
+
+            //  Bt_Read_Card.Enabled = true;
+        }
+
+        private void CardID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
     }
 }
